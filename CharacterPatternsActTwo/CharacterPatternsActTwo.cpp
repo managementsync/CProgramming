6961a0b886d50062cbf048f03af318be628357e7
managementsync <managementsync@outlook.com>
2016-10-27 11:52:15 +0200

    second char problem

--- CharacterPatternsActTwo/CharacterPatternsActTwo.cpp
+++ CharacterPatternsActTwo/CharacterPatternsActTwo.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <conio.h>
 #include <string.h>
 
 void pattern()
@@ -7,8 +7,33 @@ void pattern()
 	int ind;
 	scanf_s("%d", &ind);
 	int a, b;
-	for (int i = 1; i < ind; i++)
+	
+	for (int i = 1; i <= ind; i++)
 	{
+		scanf_s("%d %d", &a, &b);
+
+		for (int i = 0; i < a; i++)
+		{
+			for (int j = 0; j < b; j++)
+			{	
+				//first and last lines in the colums
+				if(i == 0 || i == a - 1)
+				{
+					printf("*");
+				}
+				//first and last lines in the rows
+				else if (j == 0 || j == b - 1)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(".");
+				}
+			}
+			printf("\n");
+		}
+		printf("\n");
 
 	}
 
