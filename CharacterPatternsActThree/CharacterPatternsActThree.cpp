c09076eb4a841ab5449337f4b2491bf82caf811c
managementsync <managementsync@outlook.com>
2016-10-27 14:31:46 +0200

    third

---
+++ CharacterPatternsActThree/CharacterPatternsActThree.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+void pattern()
+{
+	int ind;
+	scanf_s("%d", &ind);
+	int a, b;
+
+	for (int i = 0; i < ind; i++)
+	{
+		scanf_s("%d %d", &a, &b);
+
+		for (int i = 0; i < (a*3) + 1; i++) //fills rows
+		{
+			for (int j = 0; j < (b*3) + 1; j++) //fills colS
+			{
+				if (i % 3 == 0 || j % 3 == 0)
+				{
+
+					printf("*");
+				}
+				else
+				{
+					printf(".");
+				}
+				
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+
+	pattern();
+	_getch();
+    return 0;
+}
+
